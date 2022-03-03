13a2829fa39e558138bba8abd32954dc38d69d6c
atdetquizan <atdetquizan@gmail.com>
2022-03-02 23:05:50 -0500

    2 files modified, 9 added, 6 deleted

---
+++ src/app/slider-notices/slider-notices.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-notices',
+  templateUrl: './slider-notices.component.html',
+  styleUrls: ['./slider-notices.component.scss'],
+})
+export class SliderNoticesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
